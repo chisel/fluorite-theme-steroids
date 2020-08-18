256f20e039855ef79e6a1deeab8f504b95cdb408
Ramtin Soltani <ramtinsoltani93@gmail.com>
2020-08-18 02:45:29 -0700

    Left menu now renders a separator after parent sections and sub-sections

--- hbs-helpers.js
+++ hbs-helpers.js
@@ -2,6 +2,7 @@ module.exports = {
 
   isNotLastContent: (contents, index) => index < contents.length - 1,
   equal: (val1, val2) => val1 === val2,
-  sortVersions: versions => versions.map(v => v.version).sort().reverse().map(v => ({ version: v, link: versions.filter(ov => ov.version === v)[0].link }))
+  sortVersions: versions => versions.map(v => v.version).sort().reverse().map(v => ({ version: v, link: versions.filter(ov => ov.version === v)[0].link })),
+  hasChild: (sections, index) => sections[index + 1] ? sections[index].level < sections[index + 1].level : false
 
 };
